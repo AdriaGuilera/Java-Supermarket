d781b1f8d5a6069144191af5fe503c35b3d93067
Adrián <Adrin@DESKTOP-RR9L77J>
2024-10-23 17:49:12 +0200

    Actualización de las clases Prestatgeria y CtrlPrestatgeria

--- src/CtrlPrestatgeria.java
+++ src/CtrlPrestatgeria.java
@@ -1,2 +1,38 @@
+import java.util.Map;
+import java.util.HashMap;
+
 public class CtrlPrestatgeria {
+	private Map<String, Prestatgeria> prestatgeries;
+	
+	public CtrlPrestatgeria() {
+	 prestatgeries = new HashMap<>();
+	}
+	
+	public String afegirPrestatgeria(String id, int buits,int max, int mida_prestatge) {
+		if (id == null || id.isEmpty()) {
+            return "Error: El nom de la prestatgeria no pot estar buit.";
+        }
+		if (prestatgeries.containsKey(id)) {
+            return "Error: Ja existeix una prestatgeria amb aquest idedntificador.";
+        }
+		prestatgeries.put(id, new Prestatgeria(id,buits,max,mida_prestatge));
+		return "Prestatgeria afegida correctament";
+	}
+	
+	public String eliminarPrestatgeria(String id) {
+		 if (id == null || id.isEmpty()) {
+	            return "Error: El nom de la prestatgeria no pot estar buit.";
+	        }
+	     if (!prestatgeries.containsKey(id)) {
+	            return "Error: No existeix una prestatgeria amb aquest idedntificador.";
+	        }
+		prestatgeries.remove(id);
+		return "Prestatgeria eliminada correctament";
+	}
+	
+	public void fixarProducte(String id, String nomP) {
+		
+	}
+	
+	
 }
--- src/Prestatgeria.java
+++ src/Prestatgeria.java
@@ -1,6 +1,5 @@
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Vector;
 import java.util.HashSet;
 import java.util.Set;
 //import javafx.util.Pair;
@@ -8,56 +7,102 @@ import java.util.Set;
 
 public class Prestatgeria {
 	private String id;
-	private int buit;
+	private int buits;
 	private int mida_prestatge;
 	private int max_producte_buit;
-	//private Map<String, Pair<Integer, Integer>> productes;// productes: map<nom:string, pair(posicio:int, quantitat:int)>
-	private Map<String, Integer> productes;//mientras no va el pair usaré esto
+	private Map<String, Pair<Integer, Integer>> productes;// productes: map<nom:string, pair(posicio:int, quantitat:int)>
+	private Map<String, Boolean> productes_fixats;
 	
-	//private Vector<Pair<String, Boolean>> productes_fixats;
-	
-	public Prestatgeria(String id, int buit,int max, int mida_prestatge) {
+	public Prestatgeria(String id, int buits,int max, int mida_prestatge) {
 		this.id = id;
-		this.buit= buit;
+		this.buits= buits;
 		this.max_producte_buit= max;
 		this.mida_prestatge=mida_prestatge;
 		this.productes= new HashMap<>();
-		//this.productes_fixats= new Vector<Pair<String, Boolean>>();
+		this.productes_fixats= new HashMap<>();
 		
+	}
+	
+	public void afegir_producte(String nomP, Integer quantitat) {
+		if(productes.size() < buits) {
+			Pair<Integer, Integer> newPair= new Pair<>(0, quantitat);
+			productes.put(nomP, newPair);
+			productes_fixats.put(nomP, false);
+		}
+	}
+	public void eliminar_producte(String nomP) {
+		productes.remove(nomP);
+		productes_fixats.remove(nomP);
 	}
 	
 	public String getid() {
 		return id;
 	}
 	
-	//modificar cuando vaya el pair
-	public Map<String, Integer> getProductes(){
+	
+	public  Map<String, Pair<Integer, Integer>> getProductes(){
 		return productes;
 	}
-	//modificar cuando vaya el pair
-	public Set<Integer> auto_omplir(){
-		Set<Integer> set = new HashSet<>();
-		int cantidad;
-		for (Map.Entry<String, Integer> entry : productes.entrySet()) {
-			cantidad = entry.getValue();
-			if( cantidad < max_producte_buit) {
+	
+	public Set<Pair<String, Integer>> auto_reomplir(){
+		Set<Pair<String, Integer>> set = new HashSet<>();
+		productes.forEach((key, value) -> {
+			Pair<String, Integer> cantidad =  new Pair<>(key, value.getValue());
+			if( cantidad.getValue() < max_producte_buit) {
 				set.add(cantidad);
 			}
-		}
+		});
 		return set;
 		
 	}
-	public void afegir_prestatge() {
-		this.buit += mida_prestatge;
-	}
-	public void eliminar_prestatge() {
-		this.buit -= mida_prestatge;
+	
+	public int get_pos(String nomP){
+		return productes.get(nomP).getKey();
 	}
 	
-public void incrementar_quantitat(String id, Integer quantitat) {
-	productes.put(id, productes.get(id)+quantitat);
+	public void afegir_prestatge() {
+		this.buits += mida_prestatge;
+	}
+	public void eliminar_prestatge() {
+		this.buits -= mida_prestatge;
+	}
+	
+public void incrementar_quantitat(String nomP, Integer quantitat) {
+	Pair<Integer, Integer> currentPair = productes.get(nomP);
+
+    if (currentPair != null) {
+        // Obtenemos los valores actuales del Pair
+        int firstValue = currentPair.getKey();     // Primer valor
+        int secondValue = currentPair.getValue();  // Segundo valor
+        if(secondValue + quantitat <= max_producte_buit) {
+        	Pair<Integer, Integer> newPair = new Pair<>(firstValue, secondValue + quantitat);
+        	productes.put(nomP, newPair);
+        }
+    }
 }
-public void decrementar_quantitat(String id, Integer quantitat) {
-	productes.put(id, productes.get(id)-quantitat);
+public void decrementar_quantitat(String nomP, Integer quantitat) {
+	Pair<Integer, Integer> currentPair = productes.get(nomP);
+
+    if (currentPair != null) {
+        // Obtenemos los valores actuales del Pair
+        int firstValue = currentPair.getKey();     // Primer valor
+        int secondValue = currentPair.getValue();  // Segundo valor
+        if(secondValue - quantitat > 0) {
+        	Pair<Integer, Integer> newPair = new Pair<>(firstValue, secondValue - quantitat);
+        	productes.put(nomP, newPair);
+        }
+        else if(secondValue - quantitat == 0) {
+        	eliminar_producte(nomP);
+        	
+        }
+    }
+}
+
+public void fixar_producte_prestatgeria(String nomP) {
+	productes_fixats.put(nomP, true);
+	
+}
+public void desfixar_producte_prestatgeria(String nomP) {
+	productes_fixats.put(nomP, false);
 }
 }
